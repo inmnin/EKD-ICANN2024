143e04b65710f8d0673f7f74088ffc44a1030f0e
inmnin <130427749+inmnin@users.noreply.github.com>
2024-01-31 20:49:18 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,5 @@
 import torch
 
-
-# 验证函数
 def evaluate(model, val_dataloader, device):
     model.eval()
     val_accuracy = 0
@@ -11,7 +9,6 @@ def evaluate(model, val_dataloader, device):
             attention_mask = batch['attention_mask'].to(device)
             labels = batch['labels'].to(device)
 
-            #不要获取隐藏状态
             outputs = model(input_ids=input_ids, attention_mask=attention_mask,output_hidden_states=False)
             logits = outputs.logits
             preds = torch.argmax(logits, dim=1)
